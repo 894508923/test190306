a406e10a973536113a66932a0d8b1dedaef6409a
harrisonliu@zhaoxin.com <harrisonliu@zhaoxin.com>
2019-03-06 20:08:19 +0800

    add test2.c in testbranch

---
+++ test2.c
@@ -0,0 +1,14 @@
+/***********************************************************************************
+*              Copyright 2016 - 2020 Shanghai ZhaoXin Semiconductor Co., Ltd.
+* 
+* FileName    :   test2.c
+* Description :   
+* 
+* History     :   
+* Version         Date          Author            Description
+* 1.0             2019-03-06                      Create the file.
+***********************************************************************************/
+
+#include "test2.h"
+
+222
